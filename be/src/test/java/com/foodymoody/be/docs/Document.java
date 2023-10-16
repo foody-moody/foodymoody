e6ed78fea931defedfd02e693855ce4aad101d1b
sejeongsong <buttermelt@daum.net>
2023-10-16 15:29:11 +0900

    chore: 테스트 환경 설정 중

--- be/src/test/java/com/foodymoody/be/docs/Document.java
+++ be/src/test/java/com/foodymoody/be/docs/Document.java
@@ -2,18 +2,44 @@ package com.foodymoody.be.docs;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ExtendWith(RestDocumentationExtension.class)
+@Testcontainers
 abstract class Document {
 
     protected RequestSpecification spec;
+
+    public static final DockerImageName MYSQL_IMAGE = DockerImageName.parse("mysql:8.0.24");
+    @Container
+    public static final MySQLContainer<?> MYSQL = new MySQLContainer<>(MYSQL_IMAGE)
+            .withDatabaseName("foodymoody")
+            .withUsername("bono")
+            .withPassword("1111")
+            .withExposedPorts(3306)  // 컨테이너 내부에서 3306 포트를 노출
+            .withReuse(true);
+
+    static {
+        MYSQL.setPortBindings(List.of("3306:3306"));  // 호스트의 3308 포트를 컨테이너의 3306 포트에 바인드
+    }
 
     @BeforeEach
     void setSpec(RestDocumentationContextProvider provider) {
@@ -21,5 +47,22 @@ abstract class Document {
                 .addFilter(RestAssuredRestDocumentation.documentationConfiguration(provider))
                 .build();
     }
+
+    @BeforeAll
+    static void startContainer(){
+        MYSQL.start();
+    }
+
+//    @AfterEach
+//    void cleanUp(){
+//        MYSQL.stop();
+//        System.out.println("1");
+//    }
+
+
+//    @DynamicPropertySource
+//    public static void setMysqlProperties(DynamicPropertyRegistry registry){
+//        registry.add("spring.datasource.url",() -> "jdbc:mysql://localhost:"+MYSQL.getFirstMappedPort()+"/foodymoody");
+//    }
 
 }
--- be/src/test/java/com/foodymoody/be/docs/FeedDocument.java
+++ be/src/test/java/com/foodymoody/be/docs/FeedDocument.java
@@ -15,9 +15,76 @@ import org.springframework.restdocs.operation.preprocess.Preprocessors;
 
 public class FeedDocument extends Document {
 
-    @DisplayName("피드를 등록한다.")
+    @DisplayName("피드를 등록한다.1")
     @Test
     void registerFeed() {
+        spec.filter(document("registerFeed", Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint())));
+
+        // given
+        Map<String, Object> body = new HashMap<>();
+        body.put("review", "맛있게 먹었습니다.");
+        body.put("imageUrls", List.of("https://www.google.com/", "https://www.naver.com/"));
+        body.put("menus", List.of(Map.of("name", "마라탕", "numStar", 4), Map.of("name", "떡볶이", "numStar", 5)));
+
+        // when
+        var response = RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .spec(spec)
+                .log().all()
+                .body(body)
+                .when()
+                .post("/api/feeds")
+                .then()
+                .log().all()
+                .extract();
+
+        // then
+        Assertions.assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(200),
+                () -> assertThat(response.jsonPath().getLong("id")).isEqualTo(1)
+                //TODO 피드 조회시 등록한 피드가 조회된다.
+        );
+    }
+
+    @DisplayName("피드를 등록한다.2")
+    @Test
+    void registerFeed2() {
+        spec.filter(document("registerFeed", Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint())));
+
+        // given
+        Map<String, Object> body = new HashMap<>();
+        body.put("review", "맛있게 먹었습니다.");
+        body.put("imageUrls", List.of("https://www.google.com/", "https://www.naver.com/"));
+        body.put("menus", List.of(Map.of("name", "마라탕", "numStar", 4), Map.of("name", "떡볶이", "numStar", 5)));
+
+        // when
+        var response = RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .spec(spec)
+                .log().all()
+                .body(body)
+                .when()
+                .post("/api/feeds")
+                .then()
+                .log().all()
+                .extract();
+
+        // then
+        Assertions.assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(200),
+                () -> assertThat(response.jsonPath().getLong("id")).isEqualTo(1)
+                //TODO 피드 조회시 등록한 피드가 조회된다.
+        );
+    }
+
+
+    @DisplayName("피드를 등록한다.3")
+    @Test
+    void registerFeed3() {
         spec.filter(document("registerFeed", Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                 Preprocessors.preprocessResponse(Preprocessors.prettyPrint())));
 
