7311067e1aeebc40a39e494b7bd152694aeae8ba
ALbertIM0427 <guang7226961@naver.com>
2023-10-16 16:20:32 +0900

    test: TestContainer로 DB 설정

--- be/src/main/java/com/foodymoody/be/feed/dto/FeedRegisterResponse.java
+++ be/src/main/java/com/foodymoody/be/feed/dto/FeedRegisterResponse.java
@@ -2,7 +2,7 @@ package com.foodymoody.be.feed.dto;
 
 public class FeedRegisterResponse {
 
-    private Long id;
+    private final Long id;
 
     public FeedRegisterResponse(Long id) {
         this.id = id;
--- be/src/test/java/com/foodymoody/be/docs/Document.java
+++ be/src/test/java/com/foodymoody/be/docs/Document.java
@@ -1,21 +1,18 @@
 package com.foodymoody.be.docs;
 
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -26,19 +23,26 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 abstract class Document {
 
-    protected RequestSpecification spec;
-
-    public static final DockerImageName MYSQL_IMAGE = DockerImageName.parse("mysql:8.0.24");
+    public static final DockerImageName MYSQL_IMAGE = DockerImageName.parse("mysql:8.0");
     @Container
     public static final MySQLContainer<?> MYSQL = new MySQLContainer<>(MYSQL_IMAGE)
             .withDatabaseName("foodymoody")
             .withUsername("bono")
             .withPassword("1111")
-            .withExposedPorts(3306)  // 컨테이너 내부에서 3306 포트를 노출
             .withReuse(true);
 
     static {
-        MYSQL.setPortBindings(List.of("3306:3306"));  // 호스트의 3308 포트를 컨테이너의 3306 포트에 바인드
+        MYSQL.setPortBindings(List.of("3306:3306"));
+    }
+
+    protected RequestSpecification spec;
+
+    public static void api_문서_타이틀(String documentName, RequestSpecification specification) {
+        specification.filter(
+                document(documentName,
+                        Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()))
+        );
     }
 
     @BeforeEach
@@ -49,20 +53,7 @@ abstract class Document {
     }
 
     @BeforeAll
-    static void startContainer(){
+    static void startContainer() {
         MYSQL.start();
     }
-
-//    @AfterEach
-//    void cleanUp(){
-//        MYSQL.stop();
-//        System.out.println("1");
-//    }
-
-
-//    @DynamicPropertySource
-//    public static void setMysqlProperties(DynamicPropertyRegistry registry){
-//        registry.add("spring.datasource.url",() -> "jdbc:mysql://localhost:"+MYSQL.getFirstMappedPort()+"/foodymoody");
-//    }
-
 }
