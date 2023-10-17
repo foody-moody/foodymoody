4f5c2cefe1a9cff1c31d000d4925b3d3a94c4961
ALbertIM0427 <guang7226961@naver.com>
2023-10-17 15:55:45 +0900

    refactor: 메서드 분리

--- be/src/test/java/com/foodymoody/be/docs/auth/AuthTest.java
+++ be/src/test/java/com/foodymoody/be/docs/auth/AuthTest.java
@@ -1,17 +1,13 @@
 package com.foodymoody.be.docs.auth;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.foodymoody.be.docs.auth.AuthSteps.로그인_한다;
+import static com.foodymoody.be.docs.auth.AuthSteps.토큰과_응답코드_200을_응답한다;
 
 import com.foodymoody.be.docs.Document;
-import io.restassured.RestAssured;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AuthTest extends Document {
-
 
     @DisplayName("로그인 요청시 성공하면 토큰을 반환한다.")
     @Test
@@ -19,22 +15,10 @@ class AuthTest extends Document {
         // docs
         api_문서_타이틀("login_success", spec);
 
-        // given
-        Map<String, String> body = new HashMap<>();
-        body.put("email", "test@example.com");
-        body.put("password", "password");
-        var response = RestAssured
-                .given().spec(spec).log().all()
-                .body(body)
-                .when().post("/auth/login")
-                .then().log().all()
-                .extract();
+        // when
+        var response = 로그인_한다("test@example.com", spec);
 
         // then
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(response.jsonPath().getString("accessToken")).isNotNull(),
-                () -> assertThat(response.jsonPath().getString("refreshToken")).isNotNull()
-        );
+        토큰과_응답코드_200을_응답한다(response);
     }
 }
