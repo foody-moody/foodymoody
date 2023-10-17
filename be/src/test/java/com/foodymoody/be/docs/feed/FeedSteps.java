692498e0a8b7f1a94abbb47cd46beecabaedd39c
Sully <2dcoder@naver.com>
2023-10-17 18:50:53 +0900

    fix #20: API 명세서가 변경됨에 따라 전반적인 코드 수정

--- be/src/test/java/com/foodymoody/be/docs/feed/FeedSteps.java
+++ be/src/test/java/com/foodymoody/be/docs/feed/FeedSteps.java
@@ -6,7 +6,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
@@ -14,19 +13,28 @@ import org.springframework.http.MediaType;
 
 class FeedSteps {
 
-    public static void 응답코드가_200이고_id가_1이면_정상적으로_등록된_피드(ExtractableResponse<Response> response) {
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(200),
-                () -> assertThat(response.jsonPath().getLong("id")).isEqualTo(1)
-                //TODO 피드 조회시 등록한 피드가 조회된다.
-        );
-    }
-
     public static ExtractableResponse<Response> 피드를_등록한다(RequestSpecification spec) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("review", "맛있게 먹었습니다.");
-        body.put("imageUrls", List.of("https://www.google.com/", "https://www.naver.com/"));
-        body.put("menus", List.of(Map.of("name", "마라탕", "numStar", 4), Map.of("name", "떡볶이", "numStar", 5)));
+        Map<String, Object> body = Map.of(
+                "location", "맛있게 매운 콩볼 범계점",
+                "review", "맛있게 먹었습니다.",
+                "mood", "기쁨",
+                "images", List.of(
+                        Map.of(
+                                "imageUrl", "https://www.googles.com/",
+                                "menu", Map.of(
+                                        "name", "마라탕",
+                                        "numStar", 4
+                                )
+                        ),
+                        Map.of(
+                                "imageUrl", "https://www.google.com/",
+                                "menu", Map.of(
+                                        "name", "감자탕",
+                                        "numStar", 3
+                                )
+                        )
+                )
+        );
 
         return RestAssured
                 .given()
@@ -40,4 +48,13 @@ class FeedSteps {
                 .log().all()
                 .extract();
     }
+
+    public static void 응답코드가_200이고_id가_1이면_정상적으로_등록된_피드(ExtractableResponse<Response> response) {
+        Assertions.assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(200),
+                () -> assertThat(response.jsonPath().getLong("id")).isEqualTo(1)
+                //TODO 피드 조회시 등록한 피드가 조회된다.
+        );
+    }
+
 }
