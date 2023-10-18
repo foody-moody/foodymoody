00948d3d7bc5a79ffc0c85e7a6d7aba3da238268
JH <vvaa277@gmail.com>
2023-10-18 23:58:35 +0900

    chore: testpage추가

--- fe/src/routes/router.tsx
+++ fe/src/routes/router.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { HomePage } from 'pages/HomePage';
 import { PATH } from 'constants/path';
+
+import { HomePage } from 'pages/HomePage';
+import { TestPage } from 'pages/test/TestPage';
 
 const router = createBrowserRouter([
   {
     path: PATH.HOME,
     element: <HomePage />,
+    children: [],
+  },
+  {
+    path: PATH.TEST,
+    element: <TestPage />,
     children: [],
   },
 ]);
