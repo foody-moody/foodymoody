3ef2d5fa01ca829b49ca9b9e00598c2beaa3b767
Sully <2dcoder@naver.com>
2023-10-18 13:40:54 +0900

    feat #20: 피드 수정 로직 구현

--- be/src/main/java/com/foodymoody/be/feed/service/FeedService.java
+++ be/src/main/java/com/foodymoody/be/feed/service/FeedService.java
@@ -1,12 +1,10 @@
 package com.foodymoody.be.feed.service;
 
 import com.foodymoody.be.feed.domain.Feed;
-import com.foodymoody.be.feed.dto.request.FeedRegisterRequest;
 import com.foodymoody.be.feed.dto.request.FeedServiceRegisterRequest;
-import com.foodymoody.be.feed.dto.request.FeedUpdateRequest;
-import com.foodymoody.be.feed.dto.response.FeedRegisterResponse;
+import com.foodymoody.be.feed.dto.request.FeedServiceUpdateRequest;
 import com.foodymoody.be.feed.dto.request.ImageMenuPair;
-import com.foodymoody.be.feed.dto.response.FeedUpdateResponse;
+import com.foodymoody.be.feed.dto.response.FeedRegisterResponse;
 import com.foodymoody.be.feed.repository.FeedRepository;
 import com.foodymoody.be.feed.util.FeedMapper;
 import com.foodymoody.be.image.domain.Image;
@@ -16,13 +14,16 @@ import com.foodymoody.be.menu.util.MenuMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
 public class FeedService {
 
     private final FeedRepository feedRepository;
 
+    @Transactional
     public FeedRegisterResponse register(FeedServiceRegisterRequest request) {
         List<ImageMenuPair> imageMenuPairs = request.getImages();
         List<Menu> menus = MenuMapper.toMenu(imageMenuPairs);
@@ -31,6 +32,26 @@ public class FeedService {
         Feed feed = FeedMapper.toFeed(request, images, menus);
 
         return FeedMapper.toFeedRegisterResponse(feedRepository.save(feed));
+    }
+
+    @Transactional
+    public Long update(Long id, FeedServiceUpdateRequest request) {
+        Feed feed = feedRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 피드가 존재하지 않습니다."));
+
+        feed.setLocation(request.getLocation());
+        feed.setReview(request.getReview());
+        feed.setMood(request.getMood());
+
+        List<Image> newImages = ImageMapper.toImage(request.getImages());
+        feed.getImages().clear();
+        feed.getImages().addAll(newImages);
+
+        List<Menu> newMenus = MenuMapper.toMenu(request.getImages());  // I assume this is correct, but double-check if you're mapping images to menus here.
+        feed.getMenus().clear();
+        feed.getMenus().addAll(newMenus);
+
+        return feed.getId();
     }
 
 }
--- be/src/main/java/com/foodymoody/be/feed/util/FeedMapper.java
+++ be/src/main/java/com/foodymoody/be/feed/util/FeedMapper.java
@@ -3,6 +3,8 @@ package com.foodymoody.be.feed.util;
 import com.foodymoody.be.feed.domain.Feed;
 import com.foodymoody.be.feed.dto.request.FeedRegisterRequest;
 import com.foodymoody.be.feed.dto.request.FeedServiceRegisterRequest;
+import com.foodymoody.be.feed.dto.request.FeedServiceUpdateRequest;
+import com.foodymoody.be.feed.dto.request.FeedUpdateRequest;
 import com.foodymoody.be.feed.dto.response.FeedRegisterResponse;
 import com.foodymoody.be.image.domain.Image;
 import com.foodymoody.be.menu.domain.Menu;
@@ -18,12 +20,22 @@ public class FeedMapper {
         return new FeedRegisterResponse(savedFeed.getId());
     }
 
-    public static FeedServiceRegisterRequest toServiceRegisterRequest(FeedRegisterRequest feedRegisterRequest) {
+    public static FeedServiceRegisterRequest toServiceRegisterRequest(FeedRegisterRequest request) {
         return FeedServiceRegisterRequest.builder()
-                .location(feedRegisterRequest.getLocation())
-                .review(feedRegisterRequest.getReview())
-                .mood(feedRegisterRequest.getMood())
-                .images(feedRegisterRequest.getImages())
+                .location(request.getLocation())
+                .review(request.getReview())
+                .mood(request.getMood())
+                .images(request.getImages())
                 .build();
     }
+
+    public static FeedServiceUpdateRequest toServiceUpdateRequest(FeedUpdateRequest request) {
+        return FeedServiceUpdateRequest.builder()
+                .location(request.getLocation())
+                .review(request.getReview())
+                .mood(request.getMood())
+                .images(request.getImages())
+                .build();
+    }
+
 }
