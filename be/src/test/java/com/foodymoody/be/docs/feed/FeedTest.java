9ffbb341296e3bee9772811754862c6bd6300bfc
Sully <2dcoder@naver.com>
2023-10-18 13:52:52 +0900

    test #20: 피드 삭제 테스트 구현

--- be/src/test/java/com/foodymoody/be/docs/feed/FeedTest.java
+++ be/src/test/java/com/foodymoody/be/docs/feed/FeedTest.java
@@ -1,8 +1,9 @@
 package com.foodymoody.be.docs.feed;
 
 import static com.foodymoody.be.docs.feed.FeedSteps.응답코드가_200이고_id가_1이면_정상적으로_등록된_피드;
-import static com.foodymoody.be.docs.feed.FeedSteps.응답코드가_204라면_정상적으로_수정된_피드;
+import static com.foodymoody.be.docs.feed.FeedSteps.응답코드가_204라면_정상적으로_수정_삭제된_피드;
 import static com.foodymoody.be.docs.feed.FeedSteps.피드를_등록한다;
+import static com.foodymoody.be.docs.feed.FeedSteps.피드를_삭제한다;
 import static com.foodymoody.be.docs.feed.FeedSteps.피드를_수정한다;
 
 import com.foodymoody.be.docs.Document;
@@ -11,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 class FeedTest extends Document {
 
-    @DisplayName("피드를 등록 성공하면 응답코드 200을 반환한다")
+    @DisplayName("피드 등록에 성공하면 응답코드 200을 반환한다")
     @Test
     void when_registerFeed_then_response200() {
         // docs
@@ -24,9 +25,9 @@ class FeedTest extends Document {
         응답코드가_200이고_id가_1이면_정상적으로_등록된_피드(response);
     }
 
-    @DisplayName("피드를 수정 성공하면 응답코드 204를 반환한다.")
+    @DisplayName("피드 수정에 성공하면 응답코드 204를 반환한다.")
     @Test
-    void when_updateFeed_then_response400() {
+    void when_updateFeed_then_response204() {
         // docs
         api_문서_타이틀("updateFeed", spec);
 
@@ -38,7 +39,24 @@ class FeedTest extends Document {
         var updateResponse = 피드를_수정한다(registeredId, spec);
 
         // then
-        응답코드가_204라면_정상적으로_수정된_피드(updateResponse);
+        응답코드가_204라면_정상적으로_수정_삭제된_피드(updateResponse);
+    }
+
+    @DisplayName("피드 삭제에 성공하면 응답코드 204를 반환한다.")
+    @Test
+    void when_deleteFeed_then_response204() {
+        // docs
+        api_문서_타이틀("deleteFeed", spec);
+
+        // given
+        var registerResponse = 피드를_등록한다(spec);
+        long registeredId = registerResponse.jsonPath().getLong("id");
+
+        // when
+        var deleteResponse = 피드를_삭제한다(registeredId, spec);
+
+        // then
+        응답코드가_204라면_정상적으로_수정_삭제된_피드(deleteResponse);
     }
 
 }
