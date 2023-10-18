a9e811372bd11f3eda29851ecafa787f19dfdd17
Sully <2dcoder@naver.com>
2023-10-18 17:28:57 +0900

    test #20: 전체 피드 조회 테스트 코드 작성

--- be/src/test/java/com/foodymoody/be/feed/FeedTest.java
+++ be/src/test/java/com/foodymoody/be/feed/FeedTest.java
@@ -3,7 +3,9 @@ package com.foodymoody.be.feed;
 import static com.foodymoody.be.feed.FeedSteps.개별_피드를_조회한다;
 import static com.foodymoody.be.feed.FeedSteps.응답코드가_200이고_id가_존재하면_정상적으로_등록된_피드;
 import static com.foodymoody.be.feed.FeedSteps.응답코드가_200이고_개별_피드가_조회되면_정상적으로_등록된_피드;
+import static com.foodymoody.be.feed.FeedSteps.응답코드가_200이고_전체_피드가_조회되면_정상적으로_조회_가능한_전체_페이지;
 import static com.foodymoody.be.feed.FeedSteps.응답코드가_204라면_정상적으로_수정_삭제된_피드;
+import static com.foodymoody.be.feed.FeedSteps.전체_피드를_조회한다;
 import static com.foodymoody.be.feed.FeedSteps.피드를_등록한다;
 import static com.foodymoody.be.feed.FeedSteps.피드를_삭제한다;
 import static com.foodymoody.be.feed.FeedSteps.피드를_수정한다;
@@ -13,6 +15,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FeedTest extends Document {
+
+    @DisplayName("전체 피드 조회에 성공하면 응답코드 200을 반환한다")
+    @Test
+    void when_readAllFeed_then_response200() {
+        // docs
+        api_문서_타이틀("readAllFeed", spec);
+
+        // given
+        var registerResponse = 피드를_등록한다(spec);
+        long registeredId = registerResponse.jsonPath().getLong("id");
+
+        // when
+        var readFeedResponse = 전체_피드를_조회한다(spec);
+
+        // then
+        응답코드가_200이고_전체_피드가_조회되면_정상적으로_조회_가능한_전체_페이지(readFeedResponse);
+    }
 
     @DisplayName("피드 등록에 성공하면 응답코드 200을 반환한다")
     @Test
