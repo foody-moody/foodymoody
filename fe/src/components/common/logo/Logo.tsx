8b2e32cd2f68c831c3184f88901b2515c154102b
푸반 <86706366+gunoc@users.noreply.github.com>
2023-10-19 11:57:34 +0900

    [FE] 로고 컴포넌트 구현 (#31)
    * feat: logo컴포넌트 구현
    
    * feat: 변수명 수정

---
+++ fe/src/components/common/logo/Logo.tsx
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components';
+import { LogoLarge, LogoSmall } from '../icons';
+
+type Props = {
+  size: 's' | 'l';
+  onClick(): void;
+};
+
+export const Logo: React.FC<Props> = ({ size, onClick }) => {
+  const SelectedLogo = size === 's' ? LogoSmall : LogoLarge;
+  return (
+    <Wrapper onClick={onClick}>
+      <SelectedLogo />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  cursor: pointer;
+  width: fit-content;
+`;
