bca21efcc540a57e44fb79be091f1b64287d0ac3
ALbertIM0427 <guang7226961@naver.com>
2023-10-23 14:54:15 +0900

    feat: 댓글 등록시 피디가 존재하지 않으면 응답코드 400을 응답하는 로직 구현

--- be/src/main/java/com/foodymoody/be/comment/service/CommentService.java
+++ be/src/main/java/com/foodymoody/be/comment/service/CommentService.java
@@ -2,12 +2,21 @@ package com.foodymoody.be.comment.service;
 
 import com.foodymoody.be.comment.controller.RegisterCommentRequest;
 import com.foodymoody.be.comment.unit.CommentValidator;
+import com.foodymoody.be.common.exception.FeedIdNotExistsException;
+import com.foodymoody.be.feed.service.FeedService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CommentService {
 
+    private final FeedService feedService;
+
     public void registerComment(RegisterCommentRequest request) {
         CommentValidator.validate(request);
+        if (!feedService.exists(request.getFeedId())) {
+            throw new FeedIdNotExistsException();
+        }
     }
 }
