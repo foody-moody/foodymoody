919c083d74f7c4f576fe4d8dd52bd267550d0f17
ALbertIM0427 <guang7226961@naver.com>
2023-10-23 15:03:45 +0900

    feat: RegisterCommentRequest가 null이면 ContentNotExistsException이 발생하는 로직 구현

--- be/src/main/java/com/foodymoody/be/comment/unit/CommentValidator.java
+++ be/src/main/java/com/foodymoody/be/comment/unit/CommentValidator.java
@@ -6,6 +6,7 @@ import com.foodymoody.be.common.exception.ContentIsOver200Exception;
 import com.foodymoody.be.common.exception.ContentIsSpaceException;
 import com.foodymoody.be.common.exception.ContentNotExistsException;
 import com.foodymoody.be.common.exception.FeedIdNotExistsException;
+import com.foodymoody.be.common.exception.RegisterCommentRequestNotNullException;
 
 public class CommentValidator {
 
@@ -14,40 +15,45 @@ public class CommentValidator {
     }
 
     public static void validate(RegisterCommentRequest request) {
-        if (isNull(request)) {
+        if (request == null) {
+            throw new RegisterCommentRequestNotNullException();
+        }
+        String content = request.getContent();
+        if (isNull(content)) {
             throw new ContentNotExistsException();
         }
-        if (isEmpty(request)) {
+        if (isEmpty(content)) {
             throw new ContentIsEmptyException();
         }
-        if (isBlank(request)) {
+        if (isBlank(content)) {
             throw new ContentIsSpaceException();
         }
-        if (isOver200(request)) {
+        if (isOver200(content)) {
             throw new ContentIsOver200Exception();
         }
-        if (isZero(request)) {
+        long feedId = request.getFeedId();
+        if (isZero(feedId)) {
             throw new FeedIdNotExistsException();
         }
     }
 
-    private static boolean isZero(RegisterCommentRequest request) {
-        return request.getFeedId() == 0;
+    private static boolean isZero(long feedId) {
+        return feedId == 0;
     }
 
-    private static boolean isOver200(RegisterCommentRequest request) {
-        return request.getContent().length() > 200;
+    private static boolean isOver200(String content) {
+        return content.length() > 200;
     }
 
-    private static boolean isBlank(RegisterCommentRequest request) {
-        return request.getContent().isBlank();
+    private static boolean isBlank(String content) {
+        return content.isBlank();
     }
 
-    private static boolean isEmpty(RegisterCommentRequest request) {
-        return request.getContent().isEmpty();
+    private static boolean isEmpty(String content) {
+        return content.isEmpty();
     }
 
-    private static boolean isNull(RegisterCommentRequest request) {
-        return request.getContent() == null;
+    private static boolean isNull(String content) {
+        return content == null;
     }
 }
