5137518accdef8775fc6aaa20e9157521fec55cc
JH <vvaa277@gmail.com>
2023-10-23 21:19:43 +0900

    feat: 드롭다운 구현

---
+++ fe/src/components/common/dropdown/Dropdown.tsx
@@ -0,0 +1,78 @@
+import React, { useRef } from 'react';
+import { styled } from 'styled-components';
+import { useDetectClose } from 'hooks/useDetectClose';
+
+type Props = {
+  opener: React.ReactNode;
+  align?: 'left' | 'right';
+  children: React.ReactNode;
+};
+
+export const Dropdown: React.FC<Props> = (
+  { opener, align = 'left', children }
+) => {
+  const dropdownRef = useRef<HTMLUListElement>(null);
+  const openerRef = useRef<HTMLDivElement>(null);
+  const { isOpen, setIsOpen } = useDetectClose({
+    dropdownRef,
+    openerRef,
+  });
+
+  const handleToggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Wrapper>
+      <Opener onClick={handleToggleDropdown} ref={openerRef}>
+        {opener}
+      </Opener>
+      {isOpen && (
+        <DropdownBox
+          ref={dropdownRef}
+          $align={align}
+          onClick={handleToggleDropdown}
+        >
+          {children}
+        </DropdownBox>
+      )}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: fit-content;
+  position: relative;
+`;
+
+const DropdownBox = styled.ul<{ $align: 'left' | 'right' }>`
+  display: flex;
+  flex-direction: column;
+  width: 270px;
+  position: absolute;
+  top: 100%;
+  ${({ $align }) => $align}: 0;
+
+  border-radius: 0px 0px 40px 0px;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+  overflow: hidden;
+`;
+
+const Opener = styled.div`
+  display: flex;
+
+  width: fit-content;
+  height: fit-content;
+  align-items: center;
+
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.radius.small};
+
+  &:hover {
+    background: ${({ theme: { colors } }) => colors.bgGray200};
+  }
+`;
---
+++ fe/src/hooks/useDetectClose.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+type PropsType = {
+  dropdownRef?: React.RefObject<HTMLElement>;
+  openerRef?: React.RefObject<HTMLElement>;
+  initialValue?: boolean;
+};
+
+export const useDetectClose = ({ dropdownRef, openerRef, initialValue = false }: PropsType = {} ) => {
+
+  const [isOpen, setIsOpen] = useState(initialValue);
+
+  const handleToggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (openerRef?.current && openerRef.current.contains(event.target as Node)) {
+        return;
+      }
+
+      if (dropdownRef?.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen, openerRef, dropdownRef]);
+
+  return { isOpen, handleToggleDropdown};
+};
