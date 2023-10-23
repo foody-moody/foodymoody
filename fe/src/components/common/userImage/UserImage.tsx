374d8d40d93bf311529783ab0171bbd3cff54d68
JH <vvaa277@gmail.com>
2023-10-23 15:04:09 +0900

    feat: 이미지 url이 유효하지 않을 때, 이미지가 없을 때 기본이미지 추가

--- fe/src/components/common/userImage/UserImage.tsx
+++ fe/src/components/common/userImage/UserImage.tsx
@@ -9,21 +9,28 @@ type UserImageProps = {
 export const UserImage: React.FC<UserImageProps> = (
   { variant = 'default', imageUrl, onClick }
 ) => {
-  const randomGithubImageUrl =
-    'https://avatars.githubusercontent.com/u/63034672?v=4';
-  const userImage = imageUrl || randomGithubImageUrl;
+  const generateDefaultImage = `https://source.boringavatars.com/beam/${imageUrl}`;
+  // TODO imageUrl부분 전역으로 둔 member 아이디로 변경
+  const onErrorImage = (
+    event: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    event.currentTarget.src = generateDefaultImage;
+  };
 
   return (
     <Img
       onClick={onClick}
       $variant={variant}
-      src={userImage}
+      src={imageUrl || generateDefaultImage}
       alt="유저이미지"
+      onError={onErrorImage}
     />
   );
 };
 
-const Img = styled.img<{ $variant: 'default' | 'edit' }>`
+const Img = styled.img<{
+  $variant: 'default' | 'edit';
+}>`
   width: ${({ $variant }) => ($variant === 'default' ? '40px' : '100%')};
   height: ${({ $variant }) => ($variant === 'default' ? '40px' : '100%')};
   object-fit: cover;
