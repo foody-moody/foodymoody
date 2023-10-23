900b6b49fe182ee0d0e184c289e5e603820209ec
ALbertIM0427 <guang7226961@naver.com>
2023-10-23 14:42:20 +0900

    test: 댓글 등록 요청시 댓글이 200자를 초과하면 댓글 내용 200자 초과 예외 발생 테스트 구현

--- be/src/test/java/com/foodymoody/be/comment/service/CommentServiceTest.java
+++ be/src/test/java/com/foodymoody/be/comment/service/CommentServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.foodymoody.be.comment.controller.RegisterCommentRequest;
 import com.foodymoody.be.common.exception.ContentIsEmptyException;
+import com.foodymoody.be.common.exception.ContentIsOver200Exception;
 import com.foodymoody.be.common.exception.ContentIsSpaceException;
 import com.foodymoody.be.common.exception.ContentNotExistsException;
 import com.foodymoody.be.common.exception.FeedIdNotExistsException;
@@ -57,6 +58,19 @@ class CommentServiceTest {
                 .isInstanceOf(ContentIsSpaceException.class);
     }
 
+    @DisplayName("댓글 등록 시 댓글 내용이 200자를 초과하면 댓글 내용 200자 초과 예외가 발생한다.")
+    @Test
+    void when_register_comment_if_content_is_larger_than_200_then_throw_exception() {
+        // given
+        RegisterCommentRequest request = new RegisterCommentRequest();
+        request.setFeedId(1L);
+        request.setContent("a".repeat(201));
+
+        // when,then
+        assertThatThrownBy(() -> commentService.registerComment(request))
+                .isInstanceOf(ContentIsOver200Exception.class);
+    }
+
     @DisplayName("댓글 등록 시 피드 아이디가 없으면 피드 아이디 없음 예외가 발생한다.")
     @Test
     void when_register_comment_if_feed_id_not_exists_then_throw_exception() {
@@ -68,4 +82,6 @@ class CommentServiceTest {
         assertThatThrownBy(() -> commentService.registerComment(request))
                 .isInstanceOf(FeedIdNotExistsException.class);
     }
+
+
 }
