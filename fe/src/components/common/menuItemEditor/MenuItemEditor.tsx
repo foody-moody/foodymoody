3597ad5c659eb78c57b110e71d203a37b1a0a6a3
JH <vvaa277@gmail.com>
2023-10-24 20:34:26 +0900

    feat: menuItem UI구현

---
+++ fe/src/components/common/menuItemEditor/MenuItemEditor.tsx
@@ -0,0 +1,128 @@
+import { styled } from 'styled-components';
+import { CloseSmallIcon } from '../icon/icons';
+import { Input } from '../input/Input';
+import { StarRating } from '../starRating/StarRating';
+import { useStarRating } from 'hooks/useStarRating';
+
+type Props = {
+  id: number;
+  menu: FeedMenuType;
+  imageBox: React.ReactNode;
+  children: React.ReactNode;
+  onClick?: (index: number) => void;
+};
+
+export const MenuItemEditor: React.FC<Props> = (
+  { id,menu, imageBox, children, onClick }
+) => {
+
+  const {rate, handleStarClick}= useStarRating(menu.numStar);
+
+  const handleRemoveItem = (id: number) => {
+    onClick?.(id);
+  }
+
+  return (
+    <Wrapper>
+      <LeftContent>
+        {imageBox}
+        <ContentBody>
+          <Content>
+            <label htmlFor="menu">메뉴 이름</label>
+            <Input id="menu" variant="ghost" />
+          </Content>
+          <Content>
+            <label htmlFor="menu">메뉴 별점</label>
+            <StarRating index={rate}
+            onClick={handleStarClick}/>
+          </Content>
+        </ContentBody>
+      </LeftContent>
+
+      <CloseSmallIcon
+        onClick={() => {
+          handleRemoveItem(id);
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.li`
+  width: 100%;
+  display: flex;
+  box-sizing: border-box;
+  border: 1px solid ${({ theme: { colors } }) => colors.black};
+  justify-content: space-between;
+`;
+
+const LeftContent = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  width: 100%;
+  gap: 16px;
+  border: 1px solid ${({ theme: { colors } }) => colors.black};
+`;
+
+const ImageWrapper = styled.div`
+  min-width: 95px;
+  max-width: 95px;
+  min-height: 95px;
+  max-height: 95px;
+  background-color: yellow;
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 95px;
+    height: 95px;
+    background-color: rgba(0, 0, 0, 0.3);
+    /* pointer-events: none; // Make sure the overlay doesn't block interaction */
+    cursor: pointer;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10;
+  }
+`;
+
+const ContentBody = styled.div`
+  width: 100%;
+  border: 1px solid ${({ theme: { colors } }) => colors.black};
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const Content = styled.div`
+  width: 100%;
+
+  border: 1px solid ${({ theme: { colors } }) => colors.black};
+
+  display: flex;
+  flex-direction: column;
+  
+  label {
+    font: ${({ theme: { fonts } }) => fonts.displayM10};
+    color: ${({ theme: { colors } }) => colors.textPrimary};
+  border: 1px solid ${({ theme: { colors } }) => colors.black};
+
+  }
+`;
