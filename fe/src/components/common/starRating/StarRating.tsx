181b6b4c3d730177d3bd5f8c1c8d064152a8bbab
JH <vvaa277@gmail.com>
2023-10-24 20:35:48 +0900

    feat: starRating wrapper 추가, 훅으로 별점 선택 기능 분리

--- fe/src/components/common/starRating/StarRating.tsx
+++ fe/src/components/common/starRating/StarRating.tsx
@@ -1,37 +1,33 @@
-import { useState } from 'react';
+import { styled } from 'styled-components';
 import { StarLargeFillIcon, StarLargeEmptyIcon } from '../icon/icons';
 
 type Props = {
-  currentRating: number;
+  index: number;
   onClick: (rating: number) => void;
 };
 
-export const StarRating: React.FC<Props> = ({ onClick, currentRating }) => {
-  const [index, setIndex] = useState(currentRating);
-
-  const handleStarClick = (rating: number) => {
-    const selectedRating = index === rating ? 0 : rating;
-    setIndex(selectedRating);
-    onClick(selectedRating);
-  };
-
+export const StarRating: React.FC<Props> = ({ index, onClick }) => {
   return (
-    <>
+    <Wrapper>
       {[...Array(5)].map((_, i) => {
         const starIndex = i + 1;
 
         return starIndex <= index ? (
           <StarLargeFillIcon
             key={starIndex}
-            onClick={() => handleStarClick(starIndex)}
+            onClick={() => onClick(starIndex)}
           />
         ) : (
           <StarLargeEmptyIcon
             key={starIndex}
-            onClick={() => handleStarClick(starIndex)}
+            onClick={() => onClick(starIndex)}
           />
         );
       })}
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+`;
