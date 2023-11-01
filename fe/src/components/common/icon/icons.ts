94c26dcb92cffc4d5d5ae8e42d763494414cf2dc
JH <vvaa277@gmail.com>
2023-11-01 14:21:54 +0900

    chore: 플러스 아이콘 추가

--- fe/src/components/common/icon/icons.ts
+++ fe/src/components/common/icon/icons.ts
@@ -22,6 +22,7 @@ export { default as MapPinLargeIcon } from 'assets/mapPinLarge.svg?react';
 export { default as MapPinSmallIcon } from 'assets/mapPinSmall.svg?react';
 export { default as MenuIcon } from 'assets/menu.svg?react';
 export { default as PencilLineIcon } from 'assets/pencilLine.svg?react';
+export { default as PlusOutlineIcon } from 'assets/plusOutline.svg?react';
 export { default as PlusIcon } from 'assets/plus.svg?react';
 export { default as PlusGhostIcon } from 'assets/plusGhost.svg?react';
 export { default as SearchIcon } from 'assets/search.svg?react';
