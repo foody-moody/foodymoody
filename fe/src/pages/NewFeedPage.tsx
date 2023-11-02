fe1be6120e4f36374d73b7a2f204d53b2fc7cc39
ᴡᴏɴᴋʏᴜɴɢ ᴍɪɴ <mwk0725@gmail.com>
2023-11-02 17:30:43 +0900

    [FE] NewFeedPage 추가 (#127)
    * fix: Carousel width 100% 추가
    
    * feat: HeaderMob 스타일, 위치 수정
    
    * feat: custom scroll 스타일 분리
    
    * feat: 스크롤 막는 hook 추가
    
    * feat: Dim 컴포넌트 추가
    
    * feat: DetailModalPage router background로 구현
    
    * feat: NewFeedPage 추가

--- fe/src/pages/NewFeedPage.tsx
+++ fe/src/pages/NewFeedPage.tsx
@@ -1,3 +1,73 @@
+import { styled } from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
+import { media } from 'styles/mediaQuery';
+import { TextButton } from 'components/common/button/TextButton';
+import { Dim } from 'components/common/dim/Dim';
+import { Input } from 'components/common/input/Input';
+import { MenuItemEditor } from 'components/common/menuItemEditor/MenuItemEditor';
+import { useMenuItem } from 'hooks/useMenuItem';
+import { usePageNavigator } from 'hooks/usePageNavigator';
+
 export const NewFeedModalPage = () => {
-  return <>새 피드글 등록 모달</>;
+  const { navigateToHome } = usePageNavigator();
+
+  return (
+    <>
+      <Dim onClick={navigateToHome} />
+      <Wrapper>
+        <Top>
+          <TextButton color="black">취소</TextButton>
+          <TextButton color="black">게시</TextButton>
+        </Top>
+        <Form>
+          <Div>
+            <Title>가게 이름을 작성해주세요</Title>
+            <Input variant="underline" />
+          </Div>
+          <Div>
+            <Title>리뷰를 작성 할 메뉴를 등록해주세요</Title>
+          </Div>
+        </Form>
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: 16px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  border: 1px solid black;
+  background-color: #fff;
+
+  /* ${media.md} {
+    max-width: 580px;
+    min-width: 379px;
+    width: 100%;
+    max-height: 88dvh;
+    height: 100%;
+    overflow: hidden;
+  } */
+`;
+
+const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+const Form = styled.div``;
+
+const Title = styled.div`
+  font: ${({ theme: { fonts } }) => fonts.displayB16};
+  color: ${({ theme: { colors } }) => colors.black};
+`;
+
+const Div = styled.div`
+  font: ${({ theme: { fonts } }) => fonts.displayB16};
+  color: ${({ theme: { colors } }) => colors.black};
+`;
+
