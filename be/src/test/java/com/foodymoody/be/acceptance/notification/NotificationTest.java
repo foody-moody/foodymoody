c96ce22d2904d288b8625eadcbf8362d3d503c6d
ALbertIM0427 <guang7226961@naver.com>
2023-11-07 16:02:15 +0900

    refactor: 테스트 코드 간소화

--- be/src/test/java/com/foodymoody/be/acceptance/notification/NotificationTest.java
+++ be/src/test/java/com/foodymoody/be/acceptance/notification/NotificationTest.java
@@ -1,20 +1,17 @@
 package com.foodymoody.be.acceptance.notification;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.알람_발행;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.알람을_삭제한다;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.알람을_읽음으로_변경;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.유저의_모든_알람을_삭제한다;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.응답코드가_200;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.응답코드가_204;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.회원보노가_회원가입하고_아이디를_반환한다;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.회원의_모든_알람을_조회하고_첫번째_알람을_가져온다;
+import static com.foodymoody.be.acceptance.notification.NotificationSteps.회원의_모든_알람을_조회한다;
 
 import com.foodymoody.be.acceptance.AcceptanceTest;
-import com.foodymoody.be.acceptance.member.MemberSteps;
-import com.foodymoody.be.comment.domain.CommentAddNotificationEvent;
-import com.foodymoody.be.common.event.NotificationEvents;
-import com.foodymoody.be.common.event.NotificationType;
 import com.foodymoody.be.member.repository.MemberRepository;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import java.time.LocalDateTime;
-import java.util.Map;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,115 +21,58 @@ class NotificationTest extends AcceptanceTest {
 
     @Autowired
     MemberRepository memberRepository;
+    String 보노_아이디;
 
     @BeforeEach
     void setUp() {
         memberRepository.deleteAll();
+        보노_아이디 = 회원보노가_회원가입하고_아이디를_반환한다();
+        알람_발행(보노_아이디);
     }
 
     @DisplayName("알람 요청 성공하면 응답코드 200과 알람을 받는다.")
     @Test
     void when_request_notification_if_success_return_200_and_receive_notification() {
-        // given
-        String 보노_아이디 = 회원보노가_회원가입하고_아이디를_반환한다();
-        LocalDateTime createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 1);
-        CommentAddNotificationEvent event = CommentAddNotificationEvent.of(보노_아이디, "피드에 새로운 댓글이 추가했습니다",
-                NotificationType.COMMENT_ADDED,
-                createdAt);
-        NotificationEvents.publish(event);
-
         // when
-        var response = 회원의_모든_알람을_조회한다(보노_아이디);
+        var response = 회원의_모든_알람을_조회한다(보노_아이디, spec);
 
         // then
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(200)
-        );
+        응답코드가_200(response);
     }
 
-    @DisplayName("알람을 안읽음으로 요청하면 응답코드 204과 알람을 안읽음으로 변경한다.")
+    @DisplayName("알람을 읽음으로 요청하면 응답코드 204과 알람을 읽음으로 변경한다.")
     @Test
     void when_change_notification_status_if_success_then_return_204() {
         // given
-        String 보노_아이디 = 회원보노가_회원가입하고_아이디를_반환한다();
-        LocalDateTime createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 1);
-        CommentAddNotificationEvent event = CommentAddNotificationEvent.of(보노_아이디, "피드에 새로운 댓글이 추가했습니다",
-                NotificationType.COMMENT_ADDED,
-                createdAt);
-        NotificationEvents.publish(event);
-        var 알람목록 = 회원의_모든_알람을_조회한다(보노_아이디);
-        String eventId = 알람목록.jsonPath().getList("content.id", String.class).get(0);
-        Map<String, Object> body = Map.of("isRead", true);
+        var 알람_아이디 = 회원의_모든_알람을_조회하고_첫번째_알람을_가져온다(보노_아이디);
 
         // when
-        var response = RestAssured.given().log().all()
-                .body(body).contentType("application/json;charset=UTF-8").accept("application/json;charset=UTF-8")
-                .when().put("/api/notifications/{memberId}/{eventId}", 보노_아이디, eventId)
-                .then().log().all()
-                .extract();
+        var response = 알람을_읽음으로_변경(보노_아이디, 알람_아이디, spec);
 
         // then
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(204)
-        );
+        응답코드가_204(response);
     }
 
     @DisplayName("알람을 삭제하면 응답코드 204과 알람을 삭제한다.")
     @Test
     void when_delete_notification_if_success_then_return_code_204() {
         // given
-        String 보노_아이디 = 회원보노가_회원가입하고_아이디를_반환한다();
-        LocalDateTime createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 1);
-        CommentAddNotificationEvent event = CommentAddNotificationEvent.of(보노_아이디, "피드에 새로운 댓글이 추가했습니다",
-                NotificationType.COMMENT_ADDED,
-                createdAt);
-        NotificationEvents.publish(event);
-        var 알람목록 = 회원의_모든_알람을_조회한다(보노_아이디);
-        String eventId = 알람목록.jsonPath().getList("content.id", String.class).get(0);
+        var 알람_아이디 = 회원의_모든_알람을_조회하고_첫번째_알람을_가져온다(보노_아이디);
 
         // when
-        var response = RestAssured.given().log().all()
-                .when().delete("/api/notifications/{memberId}/{eventId}", 보노_아이디, eventId)
-                .then().log().all()
-                .extract();
+        var response = 알람을_삭제한다(알람_아이디, 보노_아이디, spec);
 
         // then
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(204)
-        );
+        응답코드가_204(response);
     }
 
     @DisplayName("알람을 모두 삭제하면 응답코드 204와 알람을 모두 삭제한다.")
     @Test
     void when_delete_all_notification_if_success_then_return_code_204() {
-        // given
-        String 보노_아이디 = 회원보노가_회원가입하고_아이디를_반환한다();
-        LocalDateTime createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 1);
-        CommentAddNotificationEvent event = CommentAddNotificationEvent.of(보노_아이디, "피드에 새로운 댓글이 추가했습니다",
-                NotificationType.COMMENT_ADDED,
-                createdAt);
-        NotificationEvents.publish(event);
-
         // when
-        var response = RestAssured.given().log().all()
-                .when().delete("/api/notifications/{memberId}", 보노_아이디)
-                .then().log().all()
-                .extract();
+        var response = 유저의_모든_알람을_삭제한다(보노_아이디, spec);
 
         // then
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(204)
-        );
-    }
-
-    private static ExtractableResponse<Response> 회원의_모든_알람을_조회한다(String 회원_아이디) {
-        return RestAssured.given().log().all()
-                .when().get("/api/notifications/{memberId}", 회원_아이디)
-                .then().log().all()
-                .extract();
-    }
-
-    private static String 회원보노가_회원가입하고_아이디를_반환한다() {
-        return MemberSteps.회원보노가_회원가입한다(new RequestSpecBuilder().build()).jsonPath().getString("id");
+        응답코드가_204(response);
     }
 }
