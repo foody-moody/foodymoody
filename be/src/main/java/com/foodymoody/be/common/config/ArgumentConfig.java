11d17d07610bdb84163c6bca898753606f78bee7
sejeongsong <buttermelt@daum.net>
2023-11-08 13:45:21 +0900

    refactor: 클래스 이름 변경

--- be/src/main/java/com/foodymoody/be/common/config/ArgumentConfig.java
+++ be/src/main/java/com/foodymoody/be/common/config/ArgumentConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+public class ArgumentConfig implements WebMvcConfigurer {
 
     private final MemberIdArgumentResolver memberIdArgumentResolver;
     private final MemberEmailArgumentResolver memberEmailArgumentResolver;
