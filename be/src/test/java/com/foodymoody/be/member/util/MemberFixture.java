06b66059266d8fc905e9cf247c42e95d4a8e39e9
sejeongsong <buttermelt@daum.net>
2023-11-09 14:58:02 +0900

    refactor: mood를 String에서 Mood 클래스로 변경

--- be/src/test/java/com/foodymoody/be/member/util/MemberFixture.java
+++ be/src/test/java/com/foodymoody/be/member/util/MemberFixture.java
@@ -1,9 +1,9 @@
 package com.foodymoody.be.member.util;
 
 public enum MemberFixture {
-    회원_알버트("albert@albert.com", "testtest123!", "알버트", "베지테리안", "https://www.image.com"),
-    회원_설리("sully@sully.com", "testtest123!", "설리", "베지테리안", "https://www.image.com"),
-    회원_보노("bono@bono.com", "testtest123!", "보노", "베지테리안", "https://www.image.com");
+    회원_알버트("albert@albert.com", "testtest123!", "알버트", "무드1", "https://www.image.com"),
+    회원_설리("sully@sully.com", "testtest123!", "설리", "무드1", "https://www.image.com"),
+    회원_보노("bono@bono.com", "testtest123!", "보노", "무드1", "https://www.image.com");
 
     private String id;
     private String email;
