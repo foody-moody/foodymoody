b6f1a9ff1643a3bc4741f45836e1b8276446120f
JH <vvaa277@gmail.com>
2023-11-09 18:04:16 +0900

    feat: comment 컴포넌트 구현

--- fe/src/pages/DetailFeedPage.tsx
+++ fe/src/pages/DetailFeedPage.tsx
@@ -2,14 +2,12 @@ import { styled } from 'styled-components';
 import { customScrollStyle } from 'styles/customStyle';
 import { media } from 'styles/mediaQuery';
 import { Badge } from 'components/common/badge/Badge';
-import { TextButton } from 'components/common/button/TextButton';
 import { Carousel } from 'components/common/carousel/Carousel';
 import { CommentItem } from 'components/common/commentItem/CommentItem';
 import { Dim } from 'components/common/dim/Dim';
 import { FeedAction } from 'components/common/feedAction/FeedAction';
 import { FeedUserInfo } from 'components/common/feedUserInfo/FeedUserInfo';
-import { PlusIcon } from 'components/common/icon/icons';
-import { Input } from 'components/common/input/Input';
+import { CommentInput } from 'components/common/input/CommentInput';
 import { useInput } from 'hooks/useInput';
 import { usePageNavigator } from 'hooks/usePageNavigator';
 
@@ -18,7 +16,7 @@ export const DetailFeedModalPage = () => {
   const { navigateToHome } = usePageNavigator();
   const { value, handleChange, isValid } = useInput({
     validator: (value) =>
-      value.trim().length === 0 || value.trim().length > 200,
+      value.trim().length === 0 || value.trim().length < 200,
   });
 
   const MOCK = {
@@ -99,18 +97,12 @@ export const DetailFeedModalPage = () => {
               commentCount={MOCK.commentCount}
             />
             <CommentBox>
-              <Input
-                variant="comment"
+              <CommentInput
                 value={value}
+                limitedLength={200}
                 onChangeValue={handleChange}
-                leftBtn={<PlusIcon />}
-                rightBtn={
-                  <TextButton color="orange" size="s" onClick={handleSubmit}>
-                    게시
-                  </TextButton>
-                }
+                onSubmitComment={handleSubmit}
               />
-
               <CommentItem
                 imageUrl="123"
                 nickname="댓글닉넴"
