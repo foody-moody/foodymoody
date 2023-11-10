c6939b14669f8d5547b3fdf63af2678bf76cf886
sejeongsong <buttermelt@daum.net>
2023-11-10 15:00:06 +0900

    fix: mood 컬럼을 mood_id로 수정
    - 최신 dev-be 반영

--- be/src/test/resources/data.sql
+++ be/src/test/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO mood (id, name) VALUES ('4', '무드2');
 INSERT INTO mood (id, name) VALUES ('5', '무드3');
 INSERT INTO mood (id, name) VALUES ('6', '무드4');
 
-INSERT INTO member (id, email, nickname, password, mood)
+INSERT INTO member (id, email, nickname, password, mood_id)
 VALUES ('1', 'ati@ati.com', '아티', 'ati123!', '1');
-INSERT INTO member (id, email, nickname, password, mood)
+INSERT INTO member (id, email, nickname, password, mood_id)
 VALUES ('2', 'puban@puban.com', '푸반', 'puban123!', '1');
