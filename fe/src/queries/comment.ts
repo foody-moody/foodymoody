489a3ae8d8b9364c50715eabff14665e56f6835e
JH <vvaa277@gmail.com>
2023-11-17 20:17:29 +0900

    feat: comment 에러 toast 연결

--- fe/src/queries/comment.ts
+++ fe/src/queries/comment.ts
@@ -10,7 +10,9 @@ import {
   putEditComment,
 } from 'api/comment/comment';
 import 'api/feed/feed';
+import { AxiosError } from 'axios';
 import { useMemo } from 'react';
+import { useToast } from 'recoil/toast/useToast';
 import { QUERY_KEY } from 'constants/queryKey';
 
 export const useGetComments = (id: string) => {
@@ -49,51 +51,72 @@ export const useGetComments = (id: string) => {
 
 export const usePostComment = (id: string) => {
   const queryClient = useQueryClient();
+  const toast = useToast();
 
   return useMutation({
     mutationFn: (body: NewCommentBody) => postNewComment(body),
     onSuccess: () => {
       queryClient.invalidateQueries([QUERY_KEY.comments, id]);
     },
-    onError: (error) => {
-      console.log('put editComment error: ', error);
-      // 바디가 없을 때
-      // 댓글이 없을 때
-      // 댓글이 비여 있으면
-      // 댓글이 공백 일때
-      // 댓글이 200자를 넘을 때
-      // 피드가 존재하지 않을 때
-      // 피드 아이디가 없을 때
+    // 바디가 없을 때
+    // 댓글이 없을 때
+    // 댓글이 비여 있으면
+    // 댓글이 공백 일때
+    // 댓글이 200자를 넘을 때
+    // 피드가 존재하지 않을 때
+    // 피드 아이디가 없을 때
+
+    onError: (error: AxiosError<ErrorResponse>) => {
+      const errorData = error?.response?.data;
+
+      if (errorData && errorData.code === 'c005') {
+        toast.error('댓글200자까지 입력할 수 있어요');
+      } else if (errorData && errorData.code === 'g001') {
+        toast.error('댓글을 입력해주세요');
+      } else {
+        errorData && toast.error(errorData.message);
+      }
     },
   });
 };
 
 export const usePutComment = () => {
+  // 쿼리키 배열에 id: string 넣는거 고려(인피니트 쿼리때문에)
   const queryClient = useQueryClient();
+  const toast = useToast();
 
   return useMutation({
     mutationFn: (args: EditCommentArgs) => putEditComment(args.id, args.body),
     onSuccess: () => {
       queryClient.invalidateQueries([QUERY_KEY.comments]);
     },
-    onError: (error) => {
-      console.log('put editComment error: ', error);
-      //바디가 없을 때
-      //댓글이 비여 있으면
-      //댓글이 공백 일때
-      //댓글이 200자를 넘을 때
-      //댓글이 존재하지 않을 때
-      //댓글이 이미 삭제되었을 때
+
+    onError: (error: AxiosError<ErrorResponse>) => {
+      const errorData = error?.response?.data;
+
+      if (errorData && errorData.code === 'c005') {
+        toast.error('댓글200자까지 입력할 수 있어요');
+      } else if (errorData && errorData.code === 'g001') {
+        toast.error('댓글을 입력해주세요');
+      } else {
+        errorData && toast.error(errorData.message);
+      }
     },
   });
 };
 
 export const useDeleteComment = () => {
+  const toast = useToast();
+
   return useMutation({
     mutationFn: (id: string) => deleteComment(id),
     onSuccess: () => {},
-    onError: (error) => {
+
+    onError: (error: AxiosError<ErrorResponse>) => {
       console.log('delete feed error: ', error);
+      const errorData = error?.response?.data;
+
+      errorData && toast.error(errorData.message);
     },
   });
 };
