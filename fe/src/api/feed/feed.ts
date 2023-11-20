8d5545df658df0f45409538ea96c51be587566c8
JH <vvaa277@gmail.com>
2023-11-20 14:39:13 +0900

    feat: feed조회 api 함수 추가

--- fe/src/api/feed/feed.ts
+++ fe/src/api/feed/feed.ts
@@ -1,6 +1,13 @@
 import { privateApi, publicApi } from 'api/fetcher';
 import { END_POINT } from 'constants/endpoint';
 
+export const getAllFeeds = async (page = 0, size = 10) => {
+  const { data } = await publicApi.get(END_POINT.feed(), {
+    params: { page, size },
+  });
+  return data;
+};
+
 export const getFeedDetail = async (id: string) => {
   const { data } = await publicApi.get(END_POINT.feed(id));
   return data;
@@ -15,7 +22,7 @@ export const putEditFeed = async (id: string, body: NewFeedBody) => {
   const { data } = await privateApi.put(END_POINT.feed(id), body);
   return data;
 };
-// 이런식으로 하나씩 id붙이는게 나은지
+
 export const deleteFeed = async (id: string) => {
   const { data } = await privateApi.delete(END_POINT.feed(id));
   return data;
