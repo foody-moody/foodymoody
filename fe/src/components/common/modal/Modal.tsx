757e3133c0315c2a26f36973768c7a0aa9084114
JH <vvaa277@gmail.com>
2023-11-22 15:12:34 +0900

    feat: alert연결

--- fe/src/components/common/modal/Modal.tsx
+++ fe/src/components/common/modal/Modal.tsx
@@ -8,6 +8,8 @@ import {
   useRecoilValue,
   DefaultValue,
 } from 'recoil';
+import { styled } from 'styled-components';
+import { Button } from '../button/Button';
 
 /* TODO. 나중에 모달 사용할 때 분리하기 */
 
@@ -16,6 +18,7 @@ export type ModalType = keyof ModalPropsMap;
 export type ModalPropsMap = {
   test: TestModalProps;
   test2: Test2ModalProps;
+  commentAlert: CommentAlertProps;
 };
 
 export type Modal<T extends ModalType> = {
@@ -56,9 +59,51 @@ const Test2Modal: React.FC<Test2ModalProps> = ({ title, content }) => {
   );
 };
 
+// type AlertModalProps = {
+//   children: React.ReactNode;
+// };
+
+// export const AlertContainer: React.FC<AlertModalProps> = ({ children }) => {
+//   return <div>{children}
+
+//   </div>;
+// };
+
+type CommentAlertProps = {
+  onEdit(): void;
+  onDelete(): void;
+  onClose?(): void;
+};
+
+export const CommentAlert: React.FC<CommentAlertProps> = ({
+  onEdit,
+  onDelete,
+  onClose,
+}) => {
+  return (
+    <Wrapper>
+      <Button size="s" backgroundColor="black" onClick={onEdit}>
+        수정
+      </Button>
+      <Button size="s" backgroundColor="black" onClick={onDelete}>
+        삭제
+      </Button>
+      <Button size="s" backgroundColor="black">
+        신고
+      </Button>
+      {onClose && (
+        <Button size="s" backgroundColor="black" onClick={onClose}>
+          취소
+        </Button>
+      )}
+    </Wrapper>
+  );
+};
+
 const MODAL_COMPONENTS: {
   [key in ModalType]: React.ComponentType<ModalPropsMap[key]>;
 } = {
+  commentAlert: CommentAlert,
   test: TestModal,
   test2: Test2Modal,
 };
@@ -100,7 +145,7 @@ export const modalSelector = selectorFamily<Modal<ModalType>, ModalType>({
     },
 });
 
-const ModalItem: React.FC<{ id: ModalType }> = ({ id }) => {
+export const ModalItem: React.FC<{ id: ModalType }> = ({ id }) => {
   const modalData = useRecoilValue(modalSelector(id));
   if (!modalData.isOpen || !modalData.props) return null;
   const ModalComponent = MODAL_COMPONENTS[id] as React.ComponentType<
@@ -122,7 +167,7 @@ export const GlobalModals = () => {
   );
 };
 
-const useModal = <T extends ModalType>() => {
+export const useModal = <T extends ModalType>() => {
   const setModal = useRecoilCallback(
     ({ set }) =>
       (id: T, value: Omit<Modal<T>, 'id'>) => {
@@ -170,3 +215,21 @@ export const TemporaryComponent = () => {
     </div>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  border: 1px solid ${({ theme: { colors } }) => colors.black};
+  background: ${({ theme: { colors } }) => colors.white};
+  padding: 16px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  max-width: 375px;
+  min-width: 343px;
+  width: 100%;
+  height: fit-content;
+`;
