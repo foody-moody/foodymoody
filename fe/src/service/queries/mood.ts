c1779829c62643d3c1604b2b64c6202b12015d7e
JH <vvaa277@gmail.com>
2023-11-23 16:30:24 +0900

    chore: mood쿼리에 주석추가

--- fe/src/service/queries/mood.ts
+++ fe/src/service/queries/mood.ts
@@ -3,6 +3,7 @@ import { getTasteMoods } from 'service/axios/mood/mood';
 import { QUERY_KEY } from 'service/constants/queryKey';
 
 export const useGetTasteMood = () =>
+  // 반환타입 설정하기
   useQuery({
     queryKey: [QUERY_KEY.tasteMood],
     queryFn: () => getTasteMoods(),
