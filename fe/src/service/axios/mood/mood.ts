97c23d9aca4398c4f0158062d093f7f32aa34a22
JH <vvaa277@gmail.com>
2023-11-23 16:23:53 +0900

    feat: mood 타입 및 요청 엔드포인트수정

--- fe/src/service/axios/mood/mood.ts
+++ fe/src/service/axios/mood/mood.ts
@@ -2,6 +2,6 @@ import { publicApi } from 'service/axios/fetcher';
 import { END_POINT } from 'service/constants/endpoint';
 
 export const getTasteMoods = async () => {
-  const { data } = await publicApi.get(END_POINT.mood);
+  const { data } = await publicApi.get(END_POINT.tasteMood);
   return data;
 };
