7d8abc92fc0cce0e651320a6c8cd116844de6411
Sully <2dcoder@naver.com>
2023-11-28 17:50:00 +0900

    test #93: 좋아요 등록 실패 테스트

--- be/src/test/java/com/foodymoody/be/acceptance/heart/HeartSteps.java
+++ be/src/test/java/com/foodymoody/be/acceptance/heart/HeartSteps.java
@@ -41,6 +41,29 @@ public class HeartSteps {
         );
     }
 
+    public static ExtractableResponse<Response> 좋아요된_피드에_또_좋아요를_한다(String feedId, String accessToken, RequestSpecification spec) {
+        Map<String, Object> body = Map.of(
+                "feedId", feedId
+        );
+
+        return RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .spec(spec)
+                .log().all()
+                .auth()
+                .oauth2(accessToken)
+                .body(body)
+                .when()
+                .post("/api/likes")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .extract();
+    }
+
+
+
     public static ExtractableResponse<Response> 좋아요_취소를_한다(String feedId, String accessToken, RequestSpecification spec) {
         Map<String, Object> body = Map.of(
                 "feedId", feedId
