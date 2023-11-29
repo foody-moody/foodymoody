b25810f591ae15661405b496f2028f4ea7b19e76
JH <vvaa277@gmail.com>
2023-11-29 19:10:21 +0900

    fix: alert에 투명 dim 추가

--- fe/src/components/common/dim/Dim.tsx
+++ fe/src/components/common/dim/Dim.tsx
@@ -3,27 +3,31 @@ import { useLockBodyScroll } from 'hooks/useLockBodyScroll';
 import { CloseLargeIcon } from '../icon/icons';
 
 type Props = {
+  isTransparent?: boolean;
   onClick: () => void;
 };
 
-export const Dim: React.FC<Props> = ({ onClick }) => {
+export const Dim: React.FC<Props> = ({ isTransparent = false, onClick }) => {
   useLockBodyScroll(true);
 
   return (
-    <Wrapper onClick={onClick}>
+    <Wrapper $isTransparent={isTransparent} onClick={onClick}>
       <CloseBtn />
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{
+  $isTransparent?: boolean;
+}>`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100dvh;
   z-index: 100;
-  background-color: ${({ theme: { colors } }) => colors.dimDark};
+  background-color: ${({ $isTransparent, theme: { colors } }) =>
+    $isTransparent ? 'transparent' : colors.dimDark};
 `;
 
 const CloseBtn = styled(CloseLargeIcon)`
