e320e9f9e83aebfe042e8e6dc5a4a74d4dd83510
Sully <2dcoder@naver.com>
2023-12-01 17:10:05 +0900

    fix #93: 좋아요 요청 수 5로 변경

--- be/src/test/java/com/foodymoody/be/acceptance/heart/HeartAcceptanceTest.java
+++ be/src/test/java/com/foodymoody/be/acceptance/heart/HeartAcceptanceTest.java
@@ -35,7 +35,7 @@ class HeartAcceptanceTest extends AcceptanceTest {
         String feedId = 피드를_등록한다(회원아티_액세스토큰, spec).jsonPath().getString("id");
 
         // when
-        int numberOfConcurrentRequests = 7;
+        int numberOfConcurrentRequests = 5;
         List<CompletableFuture<ExtractableResponse<Response>>> futures = new ArrayList<>();
 
         for (int i = 0; i < numberOfConcurrentRequests; i++) {
