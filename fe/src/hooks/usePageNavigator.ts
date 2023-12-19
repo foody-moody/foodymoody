7d9b1c073ee23c49c6238ba47202191f4750bfcb
JH <vvaa277@gmail.com>
2023-12-19 18:22:26 +0900

    feat: 비번페이지 계정페이지 설정페이지 라우트 생성

--- fe/src/hooks/usePageNavigator.ts
+++ fe/src/hooks/usePageNavigator.ts
@@ -12,6 +12,10 @@ export const usePageNavigator = () => {
     navigateToLogin: () => navigate(PATH.LOGIN),
     navigateToRegister: () => navigate(PATH.REGISTER),
     navigateToSearch: () => navigate(PATH.SEARCH),
+    navigateToProfileEdit: () => navigate(PATH.PROFILE_EDIT),
+    navigateToSetting: () => navigate(PATH.SETTING),
+    navigateToPassword: () => navigate(PATH.PASSWORD),
+    navigateToAccount: () => navigate(PATH.ACCOUNT),
     navigateToPath: (path: string) => navigate(path),
 
     /* 잠시 생성 */
