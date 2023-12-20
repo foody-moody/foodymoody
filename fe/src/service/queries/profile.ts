24c825df677c3bcb7355dbc4e9d98078573a3ee8
JH <vvaa277@gmail.com>
2023-12-20 21:11:40 +0900

    feat: profile수정 쿼리 생성

--- fe/src/service/queries/profile.ts
+++ fe/src/service/queries/profile.ts
@@ -1,18 +1,18 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { useToast } from 'recoil/toast/useToast';
 import {
   getNicknameDuplicate,
   getProfile,
+  patchEditProfile,
   patchProfileImage,
 } from 'service/axios/profile/profile';
 import { QUERY_KEY } from 'service/constants/queryKey';
 
 export const useGetProfile = (id?: string) =>
   useQuery<ProfileMemberInfo>({
-    queryKey: [QUERY_KEY.profile],
+    queryKey: [QUERY_KEY.profile, id],
     queryFn: () => getProfile(id),
-    staleTime: Infinity,
   });
 
 export const useEditProfileImage = (id: string) => {
@@ -29,11 +29,30 @@ export const useEditProfileImage = (id: string) => {
   });
 };
 
-// export const useEditProfile = () => {};
+export const useEditProfile = (memberId: string) => {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+
+  return useMutation({
+    mutationFn: (body: ProfileEditBody) => patchEditProfile(memberId, body),
+    onSuccess: () => {
+      toast.success('프로필을 수정했습니다.');
+      queryClient.invalidateQueries([QUERY_KEY.profile, memberId]); //디테일 페이지 데이터를 다시 갱신시키고 바뀐 imageId 싱크를 맞추기 위함, 이렇게해서 imageId는 항상 이전값과 같으므로 null로 보낼수있음, 이미지는 에딧 하자마자 뮤테이트 됨.
+
+      // 어디로 이동까지 시켜야할지?
+    },
+    onError: (error: AxiosError<CustomErrorResponse>) => {
+      const errorData = error?.response?.data;
+      console.log('useEditProfile error: ', error);
+
+      errorData && toast.error(errorData.message);
+    },
+  });
+};
 
 export const useGetNicknameDuplicate = (nickName: string) => {
   return useQuery({
-    queryKey: [QUERY_KEY.nickname, nickName], //여기도 닉네임을 넣어줘야하는지?
+    queryKey: [QUERY_KEY.nickname, nickName],
     queryFn: () => getNicknameDuplicate(nickName),
     enabled: false,
   });
