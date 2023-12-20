712478fb2495523f91e1f12aefa09e48e10db824
JH <vvaa277@gmail.com>
2023-12-20 21:13:35 +0900

    feat: useProfileEditForm, schema 생성

---
+++ fe/src/hooks/useProfileEditForm/usePofileEditForm.ts
@@ -0,0 +1,40 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  ProfileEditSchemaType,
+  profileEditSchema,
+} from './useProfileEditSchema';
+
+export const useProfileEditForm = (profile?: ProfileMemberInfo) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    watch,
+    trigger,
+    formState: { errors, isSubmitting },
+    clearErrors,
+    setError,
+  } = useForm<ProfileEditSchemaType>({
+    defaultValues: {
+      nickname: profile?.nickname,
+    },
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(profileEditSchema),
+  });
+
+  return {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    watch,
+    trigger,
+    errors,
+    isSubmitting,
+    clearErrors,
+    setError,
+  };
+};
---
+++ fe/src/hooks/useProfileEditForm/useProfileEditSchema.ts
@@ -0,0 +1,42 @@
+import { getNicknameDuplicate } from 'service/axios/profile/profile';
+import { z } from 'zod';
+
+export type ProfileEditSchemaType = z.infer<typeof profileEditSchema>;
+// infer: zod 스키마의 타입을 추론하기 위해 해당 스키마가 어떤 타입을 정의 하는지를 typescript에 알려주는 기능
+
+const nickNameSchema = () => {
+  return z.string().min(2, '닉네임 2자 이상 입력해주세요');
+};
+
+export const profileEditSchema = z
+  .object({
+    nickname: nickNameSchema(),
+  })
+  .refine(
+    async (value) => {
+      const response = await getNicknameDuplicate(value.nickname); // 여기를 리액트 쿼리를 쓸 순없나?
+      console.log(response, 'response');
+      return !response.isDuplicate;
+    },
+    {
+      path: ['nickname'],
+      message: '중복된 닉네임입니다.',
+    }
+  );
+
+// .refine((data) => data.password === data.passwordCheck, {
+//   path: ['passwordCheck'],
+//   message: '비밀번호가 일치하지 않습니다.',
+// })
+
+// .object({
+//   nickname: z.string().min(2, '닉네임을 입력해주세요'),
+//   password: z
+//     .string()
+//     .min(8, '비밀번호를 입력해주세요.')
+//     .regex(
+//       /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/,
+//       '영문+숫자+특수문자(! @ # $ % & * ?) 조합 8~15자리를 입력해주세요.'
+//     ),
+//   passwordCheck: z.string().min(8, '비밀번호를 입력해주세요.'),
+// })
