baadb1215ec1287c89568e2426f5376a42de4a8c
Sully <2dcoder@naver.com>
2023-12-22 18:16:53 +0900

    fix(FeedMapper) #338: toFeed 단위 테스트 재작성
    toFeed 메서드를 활용하지 않았었음

--- be/src/test/java/com/foodymoody/be/feed/application/FeedMapperTest.java
+++ be/src/test/java/com/foodymoody/be/feed/application/FeedMapperTest.java
@@ -1,54 +1,120 @@
 package com.foodymoody.be.feed.application;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.foodymoody.be.common.util.ids.FeedId;
 import com.foodymoody.be.common.util.ids.IdFactory;
 import com.foodymoody.be.common.util.ids.MemberId;
+import com.foodymoody.be.common.util.ids.StoreMoodId;
+import com.foodymoody.be.feed.application.dto.request.FeedRegisterRequestMenu;
+import com.foodymoody.be.feed.application.dto.request.FeedServiceRegisterRequest;
+import com.foodymoody.be.feed.application.dto.request.ImageMenuPair;
 import com.foodymoody.be.feed.domain.entity.Feed;
 import com.foodymoody.be.feed.domain.entity.StoreMood;
 import com.foodymoody.be.image.domain.Image;
 import com.foodymoody.be.menu.domain.Menu;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FeedMapperTest {
 
-    @DisplayName("피드 등록에서의 Feed Entity 객체를 만들고 받아올 수 있다.")
+    @DisplayName("toFeed를 활용하여 피드 등록에서의 Feed Entity 객체를 만들고 받아올 수 있다.")
     @Test
     void toFeed() {
         // given
-        FeedId feedId = IdFactory.createFeedId();
-        MemberId memberId = IdFactory.createMemberId();
-        String location = "중동";
-        String review = "맛있어요";
-        List<StoreMood> storeMoods = List.of(new StoreMood(IdFactory.createStoreMoodId(), "가족과 함께"),
-                new StoreMood(IdFactory.createStoreMoodId(), "혼밥"));
-        List<Image> images = List.of(new Image(IdFactory.createImageId("1"),
-                        "https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/foodymoody_logo.png1", memberId),
-                new Image(IdFactory.createImageId("2"),
-                        "https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/foodymoody_logo.png2", memberId));
-        List<Menu> menus = List.of(new Menu(IdFactory.createMenuId(), "라면", 5),
-                new Menu(IdFactory.createMenuId(), "짬뽕", 4));
-        String profileImageUrl = "https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/foodymoody_logo.png1";
+        FeedId id = makeFeedId();
+        MemberId memberId = makeMemberId();
+        String location = makeLocation();
+        String review = makeReview();
+        List<StoreMoodId> storeMoodIds = List.of(IdFactory.createStoreMoodId("1"), IdFactory.createStoreMoodId("2"));
+        FeedServiceRegisterRequest feedServiceRegisterRequest = new FeedServiceRegisterRequest(memberId, location,
+                review, storeMoodIds,
+                List.of(
+                        new ImageMenuPair(IdFactory.createImageId("1"),
+                                new FeedRegisterRequestMenu("라면", 5)),
+                        new ImageMenuPair(IdFactory.createImageId("2"),
+                                new FeedRegisterRequestMenu("짬뽕", 4))));
+        List<StoreMood> storeMoods = makeStoreMoods();
+        List<Image> images = makeImages(memberId);
+        List<Menu> menus = makeMenus();
+        String profileImageUrl = makeProfileImageUrl();
 
         // when
-        Feed feed = new Feed(feedId, memberId, location, review, storeMoods, images, menus, profileImageUrl,
-                LocalDateTime.MIN);
+        Feed feed = FeedMapper.toFeed(id, memberId, feedServiceRegisterRequest, storeMoods,
+                images, menus, profileImageUrl);
 
         // then
         assertAll(
-                () -> assertThat(feed.getId()).isEqualTo(feedId),
+                () -> assertThat(feed.getId()).isEqualTo(id),
                 () -> assertThat(feed.getMemberId()).isEqualTo(memberId),
                 () -> assertThat(feed.getLocation()).isEqualTo(location),
                 () -> assertThat(feed.getReview()).isEqualTo(review),
                 () -> assertThat(feed.getStoreMoods()).isEqualTo(storeMoods),
-                () -> assertThat(feed.getProfileImageUrl()).isEqualTo(profileImageUrl),
-                () -> assertThat(feed.getCreatedAt()).isEqualTo(LocalDateTime.MIN)
+                () -> assertThat(feed.getProfileImageUrl()).isEqualTo(profileImageUrl)
         );
+    }
+
+    private Feed generateFeed() {
+        FeedId feedId = makeFeedId();
+        MemberId memberId = makeMemberId();
+        String location = makeLocation();
+        String review = makeReview();
+        List<StoreMood> storeMoods = makeStoreMoods();
+        List<Image> images = makeImages(memberId);
+        List<Menu> menus = makeMenus();
+        String profileImageUrl = makeProfileImageUrl();
+
+        return new Feed(feedId, memberId, location, review, storeMoods, images, menus, profileImageUrl,
+                LocalDateTime.MIN);
+    }
+
+    @NotNull
+    private static String makeProfileImageUrl() {
+        return "https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/foodymoody_logo.png1";
+    }
+
+    @NotNull
+    private static List<Menu> makeMenus() {
+        return List.of(new Menu(IdFactory.createMenuId(), "라면", 5),
+                new Menu(IdFactory.createMenuId(), "짬뽕", 4));
+    }
+
+    @NotNull
+    private static List<Image> makeImages(MemberId memberId) {
+        return List.of(new Image(IdFactory.createImageId("1"),
+                        "https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/foodymoody_logo.png1", memberId),
+                new Image(IdFactory.createImageId("2"),
+                        "https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/foodymoody_logo.png2", memberId));
+    }
+
+    @NotNull
+    private static List<StoreMood> makeStoreMoods() {
+        return List.of(new StoreMood(IdFactory.createStoreMoodId("1"), "가족과 함께"),
+                new StoreMood(IdFactory.createStoreMoodId("2"), "혼밥"));
+    }
+
+    @NotNull
+    private static String makeReview() {
+        return "맛있어요";
+    }
+
+    @NotNull
+    private static String makeLocation() {
+        return "중동";
+    }
+
+    @NotNull
+    private static MemberId makeMemberId() {
+        return IdFactory.createMemberId();
+    }
+
+    @NotNull
+    private static FeedId makeFeedId() {
+        return IdFactory.createFeedId();
     }
 
 }
