0c032ca31617e494dce1924e03b910166be57055
JH <vvaa277@gmail.com>
2024-01-07 15:12:43 +0900

    feat: 리프레쉬 토큰 구현

--- fe/src/service/axios/fetcher.ts
+++ fe/src/service/axios/fetcher.ts
@@ -1,4 +1,15 @@
 import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import {
+  clearLoginInfo,
+  getRefreshToken,
+  setAccessToken,
+  setRefreshToken,
+  setUserInfo,
+} from 'utils/localStorage';
+import { fetchRefresh } from './auth/login';
+import { PATH } from 'constants/path';
 
 const { MODE, VITE_API_URL } = import.meta.env;
 
@@ -44,10 +55,34 @@ privateApi.interceptors.request.use(
 privateApi.interceptors.response.use(
   (response) => response,
   async (error) => {
-    console.log(error.response.status, '페쳐 error.response.status');
     console.log(error.response, '페쳐 error.response');
+    if (error.response.status === 401) {
+      const token = getRefreshToken();
+      if (!token) {
+        return Promise.reject(error);
+      }
 
+      const { accessToken, refreshToken } = await fetchRefresh(token);
+      const payload = jwtDecode(accessToken);
+
+      setAccessToken(accessToken);
+      setRefreshToken(refreshToken);
+      setUserInfo(JSON.stringify(payload));
+      console.log('리프레시 토큰 다시받아서 요청 보낼거임');
+
+      error.config.headers['Authorization'] = `Bearer ${accessToken}`;
+      return privateApi.request(error.config);
+    } else if (error.response.data.code === 'a002') {
+      console.log('리프레시 토큰 만료~~ 에러 ');
+      //리프레쉬토큰 만료
+      clearLoginInfo();
+
+      const navigate = useNavigate();
+      navigate(PATH.HOME, { replace: true });
+    }
     // TODO 리프레시 토큰 및 에러
+    // 에러바운더리 캐치
+    console.log('그냥 에러 에러바운더리 ');
 
     return Promise.reject(error);
   }
