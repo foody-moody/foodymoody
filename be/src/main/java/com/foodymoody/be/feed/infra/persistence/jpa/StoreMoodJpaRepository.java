75c50a81af6c55b4b773056cce42b0feafb30148
Sully <2dcoder@naver.com>
2024-01-19 16:04:19 +0900

    refactor(StoreMood) #442: Response로 가져올 때 쿼리 사용하여 리팩토링

--- be/src/main/java/com/foodymoody/be/feed/infra/persistence/jpa/StoreMoodJpaRepository.java
+++ be/src/main/java/com/foodymoody/be/feed/infra/persistence/jpa/StoreMoodJpaRepository.java
@@ -1,8 +1,17 @@
 package com.foodymoody.be.feed.infra.persistence.jpa;
 
 import com.foodymoody.be.common.util.ids.StoreMoodId;
+import com.foodymoody.be.feed.application.dto.response.StoreMoodResponse;
 import com.foodymoody.be.feed.domain.entity.StoreMood;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StoreMoodJpaRepository extends JpaRepository<StoreMood, StoreMoodId> {
+
+    @Query("SELECT NEW com.foodymoody.be.feed.application.dto.response.StoreMoodResponse(sm.id, sm.name) "
+            + "FROM StoreMood sm")
+    Optional<List<StoreMoodResponse>> fetchAll();
+
 }
