f0bf6b68bcf828a86f71a13130335c2dfdd3149b
JH <vvaa277@gmail.com>
2024-01-23 00:32:45 +0900

    chore: 주석제거

--- fe/src/pages/HomePage.tsx
+++ fe/src/pages/HomePage.tsx
@@ -11,7 +11,6 @@ export const HomePage = () => {
   const location = useLocation();
   const background = location.state && location.state.background;
   const isDetailFeedUrl = location.pathname.includes('/detail/feed');
-  console.log(isDetailFeedUrl, 'isDetailFeedUrl, 배포버전확인(카카오공유때문)');
 
   return (
     <Wrapper>
