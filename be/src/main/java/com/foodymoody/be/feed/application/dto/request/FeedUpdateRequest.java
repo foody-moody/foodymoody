3bfe21c809ce24a9d562370607a4b87db788ec43
Sully <2dcoder@naver.com>
2024-01-22 15:39:22 +0900

    feat(Feed) #431: Feed 수정 Request 검증 추가

--- be/src/main/java/com/foodymoody/be/feed/application/dto/request/FeedUpdateRequest.java
+++ be/src/main/java/com/foodymoody/be/feed/application/dto/request/FeedUpdateRequest.java
@@ -3,6 +3,9 @@ package com.foodymoody.be.feed.application.dto.request;
 import com.foodymoody.be.common.util.ids.StoreId;
 import com.foodymoody.be.common.util.ids.StoreMoodId;
 import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +17,14 @@ import lombok.Setter;
 @Setter
 public class FeedUpdateRequest {
 
+    @NotNull(message = "가게 정보는 필수입니다.")
     private StoreId storeId;
+    @NotBlank
+    @Size(min = 1, max = 500, message = "가게 리뷰는 최소 1글자부터 최대 500글자까지여야 합니다.")
     private String review;
+    @Size(min = 1, max = 3, message = "무드는 최소 1개부터 최대 3개까지입니다.")
     private List<StoreMoodId> storeMoodIds;
+    @Size(min = 1, message = "메뉴는 1개 이상이어야 합니다.")
     private List<ImageMenuPair> images;
 
     @Builder
