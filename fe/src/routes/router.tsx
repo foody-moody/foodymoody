bbdbbf0c731c3e0d2201cc4a985baf7d1963b572
JH <vvaa277@gmail.com>
2024-03-06 23:27:56 +0900

    feat: 디테일 페이지 청크 분리 및 테스트 배포

--- fe/src/routes/router.tsx
+++ fe/src/routes/router.tsx
@@ -1,8 +1,9 @@
+import loadable from '@loadable/component';
 import { createBrowserRouter } from 'react-router-dom';
 import { AccountSettingPage } from 'pages/AccountSettingPage';
 import { CollectionDetailPage } from 'pages/CollectionDetailPage';
 import { CollectionPage } from 'pages/CollectionPage';
-import { DetailFeedModalPage } from 'pages/DetailFeedPage';
+// import { DetailFeedModalPage } from 'pages/DetailFeedPage';
 import { ErrorPage } from 'pages/ErrorPage';
 import { FollowModalPage } from 'pages/FollowPage';
 import { HomePage } from 'pages/HomePage';
@@ -20,7 +21,24 @@ import { RegisterPage } from 'pages/RegisterPage';
 import { SearchPage } from 'pages/SearchPage';
 import { SettingPage } from 'pages/SettingPage';
 import { StorePage } from 'pages/StorePage';
+import { Spinner } from 'components/common/loading/spinner';
 import { PATH } from 'constants/path';
+
+const SpinnerPage = () => {
+  return (
+    <div style={{ width: '100%', height: '100%', background: 'red' }}>
+      <Spinner isLoading />
+    </div>
+  );
+};
+
+const DetailFeedModalPage = loadable(
+  () =>
+    import('pages/DetailFeedPage').then((module) => module.DetailFeedModalPage),
+  {
+    fallback: <SpinnerPage />,
+  }
+);
 
 const router = createBrowserRouter([
   {
