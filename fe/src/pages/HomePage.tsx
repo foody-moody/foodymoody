80c4c59d4bb4c838bb9b56f98ec4296bf0d81597
JH <vvaa277@gmail.com>
2024-03-18 19:14:17 +0900

    feat: 메타태그 수정

--- fe/src/pages/HomePage.tsx
+++ fe/src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import loadable from '@loadable/component';
 import { Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
 import { styled } from 'styled-components';
-import { CustomHelmet } from 'components/common/helmet/Helmet';
 import { DeferredComponent } from 'components/common/skeleton/DeferredComponent';
 import { FeedSkeleton } from 'components/common/skeleton/FeedSkeleton';
 import { FeedMainList } from 'components/feedMain/FeedMainList';
@@ -25,28 +24,6 @@ export const HomePage = () => {
 
   return (
     <Wrapper>
-      <CustomHelmet
-        meta={{
-          keywords:
-            '푸디무디, 푸디무디 홈, 푸디무디 메인, 푸디무디 피드, 맛집, 리뷰, 후기, 푸디무디, 푸디모디, 포디모디, 포디무디, foodymoody, 음식, 식당, 맛집추천, 맛집리뷰, 맛집탐방, 맛집투어, 내돈내산, 진짜, 존맛,  위치, 주차, 예약',
-          description: `'진짜'만 모은 맛잘알들의 맛집 컬렉션!
-          맛집 컬렉터들의 '진짜' 맛집 후기를 모아보고, 나만의 맛집 컬렉션을 만들어보세요!`,
-          ogTitle: '푸디무디 - 홈',
-          ogDescription: `여기를 눌러 링크를 확인하세요`,
-          ogImage:
-            'https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/images/fm-logo-with-text.png',
-          ogUrl: 'https://foodymoody.site',
-          ogType: 'website',
-          ogSiteName: '푸디무디',
-          ogLocale: 'ko_KR',
-          twitterTitle: '푸디무디 - 홈',
-          twitterDescription: `'진짜'만 모은 맛잘알들의 맛집 컬렉션!
-          맛집 컬렉터들의 '진짜' 맛집 후기를 모아보고, 나만의 맛집 컬렉션을 만들어보세요!`,
-          twitterImage:
-            'https://foodymoody-test.s3.ap-northeast-2.amazonaws.com/images/fm-logo-with-text.png',
-          twitterUrl: 'https://foodymoody.site',
-        }}
-      />
       <Suspense
         fallback={
           <DeferredComponent>
