baa70578cbcec51f66e32d3363e45cf65ff2acea
푸반 <86706366+gunoc@users.noreply.github.com>
2024-03-28 16:57:03 +0900

    [FE] 임시 이미지 최적화  (#623)
    * chore: 이미지리사이저 설치
    
    * feat: 이미지 사이즈 조절 로직 추가

---
+++ fe/src/utils/resizeImage.ts
@@ -0,0 +1,27 @@
+import Resizer from 'react-image-file-resizer';
+
+type Props = {
+  file: Blob;
+  maxWidth: number;
+  maxHeight: number;
+};
+
+export const resizeImage = ({ file, maxWidth, maxHeight }: Props) => {
+  const format = file.name.slice(file.name.lastIndexOf('.') + 1).toLowerCase();
+  const compressFormat = format === 'png' ? 'png' : 'jpeg';
+
+  return new Promise((res) => {
+    Resizer.imageFileResizer(
+      file,
+      maxWidth,
+      maxHeight,
+      compressFormat,
+      100,
+      0,
+      (uri) => {
+        res(uri);
+      },
+      'file'
+    );
+  });
+};
