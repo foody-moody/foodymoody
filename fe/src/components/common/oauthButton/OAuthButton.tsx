e00cde01371f78f5e03cebc2a0eb11ad9f5f1b61
devbattery <devbattery@outlook.com>
2024-12-11 10:35:25 +0900

    fix(fe) #637: VITE의 redirect url 변경 후 로그 추가
    - 원래 수정했던 VITE의 redirect url은 백엔드에서 사용하는 거라서 삭제
    - 원래 있던 코드에서 redirect address가 어떻게 넘어오는지 보기 위해 로그 추가

--- fe/src/components/common/oauthButton/OAuthButton.tsx
+++ fe/src/components/common/oauthButton/OAuthButton.tsx
@@ -1,15 +1,22 @@
 import { styled } from 'styled-components';
 import { GoogleIcon } from '../icon/icons';
-// import { PATH } from 'constants/path';
+import { PATH } from 'constants/path';
 
-const { MODE, VITE_GOOGLE_CLIENT_ID, VITE_REDIRECT_ADDRESS } = import.meta.env;
+const { MODE, VITE_GOOGLE_CLIENT_ID } = import.meta.env;
 
 export const OAuthButton = () => {
     const isDev = MODE === 'development';
     console.log('isDev', isDev);
 
-    // const LOCAL_URL = 'http://localhost:5173';
-    const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&client_id=${VITE_GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${VITE_REDIRECT_ADDRESS}&access_type=offline`;
+    const LOCAL_URL = 'http://localhost:5173';
+    // const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&client_id=${VITE_GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${VITE_REDIRECT_ADDRESS}&access_type=offline`;
+
+    console.log('RedirectAddress', 'https://foodymoody.store' + PATH.GOOGLE);
+
+    const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&client_id=${VITE_GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${
+        isDev ? LOCAL_URL + PATH.GOOGLE : 'https://foodymoody.store' + PATH.GOOGLE
+    }&access_type=offline`;
+
     // const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?
     // 	client_id=${VITE_GOOGLE_CLIENT_ID}
     // 	&redirect_uri=${isDev ? LOCAL_URL + PATH.GOOGLE : VITE_API_URL + PATH.GOOGLE}
