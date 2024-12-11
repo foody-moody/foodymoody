fc85a63cf50c6e40ccc24f8dc75150be9d5671f8
devbattery <devbattery@outlook.com>
2024-12-11 10:03:52 +0900

    fix(fe) #637: OAuth의 client_id와 redirect_url를 VITE로 받도록 수정
    - 쓰이지 않는 변수는 주석 처리 (tsconfig.json 파일에서 noUnusedLocals 또는 noUnusedParameters 옵션이 true로 설정되어 있으면, 사용되지 않는 지역 변수나 함수 매개변수에 대해 오류를 발생시킴)
    - 앞으로는 .ENV 파일에서 관리하도록 변경
    - client_secret은 프론트엔드에서 관리할 필요 없음

--- fe/src/components/common/oauthButton/OAuthButton.tsx
+++ fe/src/components/common/oauthButton/OAuthButton.tsx
@@ -1,52 +1,52 @@
 import { styled } from 'styled-components';
 import { GoogleIcon } from '../icon/icons';
-import { PATH } from 'constants/path';
+// import { PATH } from 'constants/path';
 
-const { MODE, VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_CLIENT_SECRET } = import.meta.env;
+const { MODE, VITE_GOOGLE_CLIENT_ID, VITE_REDIRECT_ADDRESS } = import.meta.env;
 
 export const OAuthButton = () => {
-  const isDev = MODE === 'development';
-  console.log('isDev', isDev);
+    const isDev = MODE === 'development';
+    console.log('isDev', isDev);
 
-  const LOCAL_URL = 'http://localhost:5173';
-  const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&client_id=${VITE_GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${VITE_GOOGLE_CLIENT_SECRET}&access_type=offline`;
-  // const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?
-  // 	client_id=${VITE_GOOGLE_CLIENT_ID}
-  // 	&redirect_uri=${isDev ? LOCAL_URL + PATH.GOOGLE : VITE_API_URL + PATH.GOOGLE}
-  // 	&response_type=code
-  // 	&scope=email profile`;
-  const handleOauthLogin = () => {
-    location.replace(GOOGLE_URL); // 이동
-    // window.open(GOOGLE_URL, '_blank', 'width=500,height=600,left=50,top=10'); // 새창
-  };
+    // const LOCAL_URL = 'http://localhost:5173';
+    const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&client_id=${VITE_GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${VITE_REDIRECT_ADDRESS}&access_type=offline`;
+    // const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?
+    // 	client_id=${VITE_GOOGLE_CLIENT_ID}
+    // 	&redirect_uri=${isDev ? LOCAL_URL + PATH.GOOGLE : VITE_API_URL + PATH.GOOGLE}
+    // 	&response_type=code
+    // 	&scope=email profile`;
+    const handleOauthLogin = () => {
+        location.replace(GOOGLE_URL); // 이동
+        // window.open(GOOGLE_URL, '_blank', 'width=500,height=600,left=50,top=10'); // 새창
+    };
 
-  return (
-    <Wrapper onClick={handleOauthLogin} type="button">
-      <GoogleIcon />
-      <Text>Google로 계속하기</Text>
-    </Wrapper>
-  );
+    return (
+        <Wrapper onClick={handleOauthLogin} type="button">
+            <GoogleIcon />
+            <Text>Google로 계속하기</Text>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.button`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  height: 48px;
-  padding: 16px;
-  border-radius: 4px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    height: 48px;
+    padding: 16px;
+    border-radius: 4px;
 
-  cursor: pointer;
-  border: 1px solid ${({ theme: { colors } }) => colors.textTertiary};
-  background-color: ${({ theme: { colors } }) => colors.white};
-  transition: all 0.2s ease-in-out;
-  &:hover {
-    background-color: ${({ theme: { colors } }) => colors.bgGray50};
-  }
+    cursor: pointer;
+    border: 1px solid ${({ theme: { colors } }) => colors.textTertiary};
+    background-color: ${({ theme: { colors } }) => colors.white};
+    transition: all 0.2s ease-in-out;
+    &:hover {
+        background-color: ${({ theme: { colors } }) => colors.bgGray50};
+    }
 `;
 
 const Text = styled.p`
-  font: ${({ theme: { fonts } }) => fonts.displayB14};
-  color: ${({ theme: { colors } }) => colors.textSecondary};
-  flex: 1;
+    font: ${({ theme: { fonts } }) => fonts.displayB14};
+    color: ${({ theme: { colors } }) => colors.textSecondary};
+    flex: 1;
 `;
